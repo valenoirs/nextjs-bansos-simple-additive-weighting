b8b59bb4ea7a46494ad3d4de78e6b83c053ff776
valenoirs <luckyvw.general@gmail.com>
2022-08-15 10:17:51 +0800

    update kriteria

--- helper/simple-additive-weighting.ts
+++ helper/simple-additive-weighting.ts
@@ -1,56 +1,62 @@
-const percent = [40,30,20,10]
+const percent = [30, 40, 10, 10, 10];
 
-function count(arr: any){
-    let hasil: any[] = []
+function count(arr: any) {
+  let hasil: any[] = [];
 
-    arr.forEach((element:any, index: any) => {
-        index++
-        if(index === 5){
-            return
-        }
+  console.log(arr);
 
-        if(index === 1){
-            let min = Math.min(...arr[index])
-            hasil.push(arr[index].map((e: any) => min/e))
-        }
-        else{
-            let max = Math.max(...arr[index])
-            hasil.push(arr[index].map((e: any) => e/max))
-        }
-    })
-        
-    let output = []
-    let real = 0
-
-    for(let i = 0; i < hasil[0].length; i++){
-        real = 0
-        for(let j = 0; j < hasil.length; j++){
-            if(j === 0){
-                real = real - (hasil[j][i] * percent[j])
-            }
-            else{
-                real = real + (hasil[j][i] * percent[j])
-            }
-        }
-        if(real < 0){
-            real = 0;
-        }
-        output.push({name: arr[0][i],score: Math.round(real)})
+  arr.forEach((element: any, index: any) => {
+    index++;
+    console.log(arr[index]);
+    if (index === 6) {
+      return;
     }
 
-    return output.sort((a, b) => {
-        return b.score - a.score
-    })
+    if (index === 1 || index === 2 || index === 4) {
+      let min = Math.min(...arr[index]);
+      hasil.push(arr[index].map((e: any) => min / e));
+    } else {
+      let max = Math.max(...arr[index]);
+      hasil.push(arr[index].map((e: any) => e / max));
+    }
+  });
+
+  let output = [];
+  let real = 0;
+
+  for (let i = 0; i < hasil[0].length; i++) {
+    real = 0;
+    for (let j = 0; j < hasil.length; j++) {
+      if (j === 0) {
+        real = real - hasil[j][i] * percent[j];
+      } else {
+        real = real + hasil[j][i] * percent[j];
+      }
+    }
+    if (real < 0) {
+      real = 0;
+    }
+    output.push({ name: arr[0][i], score: Math.round(real) });
+  }
+
+  console.log(hasil);
+  console.log(output);
+
+  return output.sort((a, b) => {
+    return b.score - a.score;
+  });
 }
 
-export default function processArray (arr: any) {
-    const result: any[] = []
+export default function processArray(arr: any) {
+  const result: any[] = [];
 
-    Object.keys(arr[0]).forEach((element,i) => {
-        result.push(arr.map((e: any) => e[element]))
-    })
+  Object.keys(arr[0]).forEach((element, i) => {
+    result.push(arr.map((e: any) => e[element]));
+  });
 
-    const output = count(result)
+  const output = count(result);
 
-    return output
+  console.log(output);
+
+  return output;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,83 +1,101 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { useState } from 'react'
-import { IOption } from '../interface/option'
-import saw from '../helper/simple-additive-weighting'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useState } from "react";
+import { IOption } from "../interface/IOption";
+import saw from "../helper/simple-additive-weighting";
 
-const penghasilan: IOption[] = [
-  {value: 1, label: 'Rp. 2.000.000 - Rp. 1.500.000'},
-  {value: 2, label: 'Rp. 1.500.000 - Rp. 1.000.000'},
-  {value: 3, label: 'Rp. 1.000.000 - Rp. 500.000'},
-  {value: 4, label: 'Rp. 500.000 - Rp. 0'},
-]
+const tempatTinggal: IOption[] = [
+  { value: 1, label: "Milik Sendiri" },
+  { value: 2, label: "Milik Orang Tua" },
+  { value: 3, label: "Kos" },
+  { value: 4, label: "Kontrak" },
+  { value: 5, label: "Tidak Ada" },
+];
 
-const status: IOption[] = [
-  {value: 1, label: 'Belum Kawin'},
-  {value: 2, label: 'Kawin'},
-  {value: 3, label: 'Cerai Hidup'},
-  {value: 4, label: 'Cerai Mati'},
-]
+const pekerjaan: IOption[] = [
+  { value: 1, label: "Wiraswasta" },
+  { value: 2, label: "Petani" },
+  { value: 3, label: "Pedagang" },
+  { value: 4, label: "Buruh" },
+  { value: 5, label: "Tidak Bekerja" },
+];
 
 const tanggungan: IOption[] = [
-  {value: 1, label: 'Tidak memiliki anak'},
-  {value: 2, label: '1 / 2 anak'},
-  {value: 3, label: '3 / 4 anak'},
-  {value: 4, label: 'Lebih dari 5 anak'},
-]
+  { value: 1, label: "Tidak Ada" },
+  { value: 2, label: "1 / 2 orang" },
+  { value: 3, label: "3 / 4 orang" },
+  { value: 4, label: "5 / 6 orang" },
+  { value: 5, label: "Lebih dari 6 orang" },
+];
 
-const umur: IOption[] = [
-  {value: 1, label: '25-30 tahun'},
-  {value: 2, label: '31-40 tahun'},
-  {value: 3, label: '41-50 tahun'},
-  {value: 4, label: 'Diatas 50 tahun'},
-]
+const pendapatan: IOption[] = [
+  { value: 1, label: "<Rp. 1.000.000" },
+  { value: 2, label: "Rp. 1.000.000 - Rp. 3.000.000" },
+  { value: 3, label: "Rp. 3.000.000 - Rp. 4.000.000" },
+  { value: 4, label: "Rp. 4.000.000 - Rp. 5.000.000" },
+  { value: 5, label: ">Rp. 5.000.000" },
+];
+
+const pengeluaran: IOption[] = [
+  { value: 1, label: "<Rp. 1.000.000" },
+  { value: 2, label: "Rp. 1.000.000 - Rp. 3.000.000" },
+  { value: 3, label: "Rp. 3.000.000 - Rp. 4.000.000" },
+  { value: 4, label: "Rp. 4.000.000 - Rp. 5.000.000" },
+  { value: 5, label: ">Rp. 5.000.000" },
+];
 
 const Home: NextPage = () => {
-  const [formValue, setFormValue] = useState<any>([{
-    nama: 1,
-    penghasilan: 1,
-    status: 1,
-    tanggungan: 1,
-    umur: 1,
-  }])
+  const [formValue, setFormValue] = useState<any>([
+    {
+      nama: "Orang",
+      tempatTinggal: 1,
+      pekerjaan: 1,
+      tanggungan: 1,
+      pendapatan: 1,
+      pengeluaran: 1,
+    },
+  ]);
 
-  const[sawValue, setSawValue] = useState<any>('')
+  const [sawValue, setSawValue] = useState<any>("");
 
-  const handleChange = (i:any, e:any) => {
-    let newFormValue = [...formValue]
-    if(e.target.name === 'nama'){
-      newFormValue[i][e.target.name] = e.target.value
+  const handleChange = (i: any, e: any) => {
+    let newFormValue = [...formValue];
+    if (e.target.name === "nama") {
+      newFormValue[i][e.target.name] = e.target.value;
+    } else {
+      newFormValue[i][e.target.name] = Number(e.target.value);
     }
-    else{
-      newFormValue[i][e.target.name] = Number(e.target.value)
-    }
-    setFormValue(newFormValue)
-  }
+    setFormValue(newFormValue);
+  };
 
   const removeFormFields = (i: any) => {
     let newFormValue = [...formValue];
     newFormValue.splice(i, 1);
-    setFormValue(newFormValue)
-  }
+    setFormValue(newFormValue);
+  };
 
   const addFormFields = () => {
-    setFormValue([...formValue, {
-      nama: 1,
-      penghasilan: 1,
-      status: 1,
-      tanggungan: 1,
-      umur: 1,
-    }])
-  }
+    setFormValue([
+      ...formValue,
+      {
+        nama: "Orang",
+        tempatTinggal: 1,
+        pekerjaan: 1,
+        tanggungan: 1,
+        pendapatan: 1,
+        pengeluaran: 1,
+      },
+    ]);
+  };
 
   const handleSubmit = (e: any) => {
-    e.preventDefault()
-    let sawCalculate = saw(formValue)
+    e.preventDefault();
+    let sawCalculate = saw(formValue);
 
-    setSawValue(sawCalculate)
-  }
+    setSawValue(sawCalculate);
+  };
 
   return (
     <div className={styles.container}>
@@ -86,56 +104,124 @@ const Home: NextPage = () => {
         <meta name="description" content="valenoirs" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='center'>
+      <main className="center">
         <div className="container mx-auto p-6">
           <div className="row-auto"></div>
-          <form className='form-control w-full max-w-xs' onSubmit={handleSubmit}>
+          <h1 className="text-center text-xl mb-5">
+            Simple Additive Weighting
+          </h1>
+          <form
+            className="form-control w-full max-w-xs"
+            onSubmit={handleSubmit}
+          >
             {formValue.map((element: any, index: any) => (
-            <div key={index}>
-              
-            <label className="label">
+              <div key={index}>
+                {/* <label className="label">
               <span className="label-text">Simple Additive Weighting</span>
-            </label>
-            <div className="input-group">
-              <input type="text" className='input w-[15rem] max-w-xs' name="nama" placeholder='Nama' onChange={e => handleChange(index, e)} />
-              <select name='penghasilan' defaultValue={1} className="select w-[15rem] max-w-xs" onChange={e => handleChange(index, e)}>
-                <option disabled selected>Penghasilan</option>
-                {penghasilan.map((e,i) => 
-                  <option key={i} value={e.value}>{e.label}</option>
-                )}
-              </select>
-              <select name='status' defaultValue={1} className="select w-[15rem] max-w-xs" onChange={e => handleChange(index, e)}>
-                <option disabled selected>Status</option>
-                {status.map((e,i) => 
-                  <option key={i} value={e.value}>{e.label}</option>
-                )}
-              </select>
-              <select name='tanggungan' defaultValue={1} className="select w-[15rem] max-w-xs" onChange={e => handleChange(index, e)}>
-                <option disabled selected>Tanggungan</option>
-                {tanggungan.map((e,i) => 
-                  <option key={i} value={e.value}>{e.label}</option>
-                )}
-              </select>
-              <select name='umur' defaultValue={1} className="select w-[15rem] max-w-xs" onChange={e => handleChange(index, e)}>
-                <option disabled selected>Umur</option>
-                {umur.map((e,i) => 
-                  <option key={i} value={e.value}>{e.label}</option>
-                )}
-              </select>
-            {index ? 
-            <button type="button" className="btn btn-error" onClick={() => removeFormFields(index)}>
-              Remove
-            </button> :
-            null
-            }
-            </div>
-            </div>
+            </label> */}
+                <div className="input-group">
+                  <label htmlFor="nama" className="input-group-vertical">
+                    <span>Nama</span>
+                    <input
+                      type="text"
+                      className="input w-[15rem] max-w-xs"
+                      name="nama"
+                      placeholder="Orang"
+                      defaultValue={"Orang"}
+                      onChange={(e) => handleChange(index, e)}
+                    />
+                  </label>
+                  <label>
+                    <span>Tempat Tinggal</span>
+                    <select
+                      name="tempatTinggal"
+                      className="select w-[15rem] max-w-xs"
+                      onChange={(e) => handleChange(index, e)}
+                    >
+                      {tempatTinggal.map((e, i) => (
+                        <option key={i} value={e.value}>
+                          {e.label}
+                        </option>
+                      ))}
+                    </select>
+                  </label>
+                  <label>
+                    <span>Pekerjaan</span>
+                    <select
+                      name="pekerjaan"
+                      className="select w-[15rem] max-w-xs"
+                      onChange={(e) => handleChange(index, e)}
+                    >
+                      {pekerjaan.map((e, i) => (
+                        <option key={i} value={e.value}>
+                          {e.label}
+                        </option>
+                      ))}
+                    </select>
+                  </label>
+                  <label>
+                    <span>Tanggungan</span>
+                    <select
+                      name="tanggungan"
+                      className="select w-[15rem] max-w-xs"
+                      onChange={(e) => handleChange(index, e)}
+                    >
+                      {tanggungan.map((e, i) => (
+                        <option key={i} value={e.value}>
+                          {e.label}
+                        </option>
+                      ))}
+                    </select>
+                  </label>
+                  <label>
+                    <span>Pendapatan</span>
+                    <select
+                      name="pendapatan"
+                      className="select w-[15rem] max-w-xs"
+                      onChange={(e) => handleChange(index, e)}
+                    >
+                      {pendapatan.map((e, i) => (
+                        <option key={i} value={e.value}>
+                          {e.label}
+                        </option>
+                      ))}
+                    </select>
+                  </label>
+                  <label>
+                    <span>Pengeluaran</span>
+                    <select
+                      name="pengeluaran"
+                      className="select w-[15rem] max-w-xs"
+                      onChange={(e) => handleChange(index, e)}
+                    >
+                      {pengeluaran.map((e, i) => (
+                        <option key={i} value={e.value}>
+                          {e.label}
+                        </option>
+                      ))}
+                    </select>
+                  </label>
+                  {index ? (
+                    <button
+                      type="button"
+                      className="btn btn-error"
+                      onClick={() => removeFormFields(index)}
+                    >
+                      Remove
+                    </button>
+                  ) : null}
+                </div>
+              </div>
             ))}
             <div className="btn-group mt-10">
-              <button className="btn btn-success" type="button" onClick={() => addFormFields()}>
+              <button
+                className="btn btn-success"
+                type="button"
+                onClick={() => addFormFields()}
+              >
                 Add
               </button>
-              <button className='btn btn-accent' type='submit'>
+              <button className="btn btn-accent" type="submit">
                 Submit
               </button>
             </div>
@@ -143,7 +229,7 @@ const Home: NextPage = () => {
         </div>
 
         <div className="container mx-auto p-6">
-            {sawValue && 
+          {sawValue && (
             <div>
               <div className="overflow-x-auto">
                 <table className="table w-full">
@@ -155,22 +241,22 @@ const Home: NextPage = () => {
                     </tr>
                   </thead>
                   <tbody>
-                    {sawValue.map((e:any,i:any) => 
-                    <tr key={i}>
-                      <th>{i+1}</th>
-                      <td>{e.name}</td>
-                      <td>{e.score}</td>
-                    </tr>
-                    )}
+                    {sawValue.map((e: any, i: any) => (
+                      <tr key={i}>
+                        <th>{i + 1}</th>
+                        <td>{e.name}</td>
+                        <td>{e.score}</td>
+                      </tr>
+                    ))}
                   </tbody>
                 </table>
               </div>
             </div>
-            }
+          )}
         </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
